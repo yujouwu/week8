e51af34c6e9048495130ada91e241f5eba52c2b1
sonia <soniayjw@gmail.com>
2023-09-10 06:49:24 +0800

    update index faq

--- main.js
+++ main.js
@@ -20,3 +20,17 @@ import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.mjs
        },
      }
   )
+
+  const swiper2 = new Swiper('.swiper2', {
+  // slide 間距
+  spaceBetween: 24,
+  slidesPerView: 3,
+  // Scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+    draggable: true,
+  },
+  // 使 swiper 可以使用滑鼠滾輪捲動
+  mousewheel: true,
+
+  });
